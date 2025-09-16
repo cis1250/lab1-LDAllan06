12f073cc2b635143d56908ef682cb320d71324a0
Liam Allan <lallan03@uoguelph.ca>
2025-09-16 16:52:24 -0400

    Add name and hobby print statements

--- hello.py
+++ hello.py
@@ -6,4 +6,5 @@
 # 2. Add another print statement with your name.
 # 3. Add another print statement with your favorite hobby.
 
-print("Hello, world!")
+print("Liam Allan")
+print("My favorite hobby is playing football")
